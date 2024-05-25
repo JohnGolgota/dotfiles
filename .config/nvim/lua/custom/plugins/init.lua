559bd0948834b855b97072ce109cc716d66b6cbd
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-05-25 16:23:30 -0500

    jodes mucho gpg

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,20 +2,32 @@ return {
   "simrat39/rust-tools.nvim",
   "evanleck/vim-svelte",
   'nvim-lua/plenary.nvim',
-'nvim-pack/nvim-spectre',
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
-      vim.keymap.set('i', '<tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
+  'nvim-pack/nvim-spectre',
+  -- {
+  --   "github/copilot.vim",
+  --   config = function()
+  --     vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
+  --       expr = true,
+  --       replace_keycodes = false
+  --     })
+  --     vim.keymap.set('i', '<tab>', 'copilot#Accept("\\<CR>")', {
+  --       expr = true,
+  --       replace_keycodes = false
+  --     })
 
-      vim.g.copilot_no_tab_map = false
+  --     vim.g.copilot_no_tab_map = false
+  --   end,
+  -- },
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        ketmaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+      })
     end,
   },
 }
