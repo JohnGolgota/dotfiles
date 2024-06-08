5895c7e67f9173d866e1b99acf1e316120666762
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-06-08 15:26:54 -0500

    terrenaitor

--- .zshrc
+++ .zshrc
@@ -5,3 +5,8 @@ fi
 if [[ -f ~/shell-setup/aliases.sh ]]; then
 	source ~/shell-setup/aliases.sh
 fi
+
+eval "$(zoxide init zsh)"
+
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --zsh)"
--- shell-setup/envs.sh
+++ shell-setup/envs.sh
@@ -1,31 +1,26 @@
-
+# personal
 export MY_PATHS="~/JS/bin/private_hashtable.ps1;~/JS/bin/repo.ps1"
 
 export JS="/Users/usuario/JS"
 
 export MY_CD_CLI_PATH="~/asd/bin/asd.ps1"
 
+# gpg
 export GPG_TTY=$(tty)
 
-export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin"
+# path
+export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin:$HOME/JS/bin:$HOME/asd/bin:$HOME/.dotnet/tools"
 
-export PATH="$PATH:/Users/usuario/JS/bin"
-
-export PATH="$PATH:/Users/usuario/asd/bin"
-
+# darwin
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
-
-export PATH="$PATH:$HOME/.dotnet/tools"
 
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH=$BUN_INSTALL/bin:$PATH
-~/.bun/bin
 
 # pnpm
-export PNPM_HOME="/home/jiss/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
-# pnpm end
