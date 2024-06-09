de4da406b8686260f28213b2aef84e9eae4bebe2
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-06-09 16:49:19 -0500

    wezterm confing init

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.default_prog = { "pwsh", "-NoLogo" }
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "Batman"
+
+config.window_background_opacity = 0.6
+
+-- and finally, return the configuration to wezterm
+return config
