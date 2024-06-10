153ef108d30124701413b38960a409c98a8a6b15
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-06-09 21:43:36 -0500

    no pense en eso xd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,5 +2,7 @@ require "kickstart"
 require "config"
 
 require("lazy").setup({
-    {import = "kickstart.kickstart.plugins"}, {import = "plugins"}
+    {import = "kickstart.kickstart.plugins.default"},
+    -- {import = "kickstart.kickstart.plugins"}, 
+    {import = "plugins"}
 }, {{import = "kickstart.ui"}})
