7f6c3b3ec098cbded68bf124ad856ddeace179a0
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-01-04 10:46:21 -0500

    en windows me jodo

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.default_prog = { "pwsh", "-NoLogo" }
+config.default_prog = { os.getenv("SHELL"), "-NoLogo" }
 
 -- For example, changing the color scheme:
 -- config.color_scheme = "ayu"
