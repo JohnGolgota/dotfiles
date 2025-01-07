35df610aa688f952bf9c2fdde9eb270a0789f7da
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-01-07 14:43:27 -0500

    ya camine por la cornisa

--- run_after__symlink_windows_config.ps1
+++ run_after__symlink_windows_config.ps1
@@ -1,18 +1,28 @@
-if ($IsWindows) {
+if ($IsWindows)
+{
     $nvimConfDir = (Join-Path $env:LOCALAPPDATA "nvim")
-    if (-not (Test-Path $nvimConfDir)) {
+    if (-not (Test-Path $nvimConfDir))
+    {
         New-Item -Path $nvimConfDir -ItemType Junction -Value ${HOME}\.config\nvim
     }
     $rioConfDir = (Join-Path $env:LOCALAPPDATA "rio")
-    if (-not (Test-Path $rioConfDir)) {
+    if (-not (Test-Path $rioConfDir))
+    {
         New-Item -Path $rioConfDir -ItemType Junction -Value ${HOME}\.config\rio
     }
     $helixConfDir = (Join-Path $env:APPDATA "helix")
-    if (-not (Test-Path $helixConfDir)) {
+    if (-not (Test-Path $helixConfDir))
+    {
         New-Item -Path $helixConfDir -ItemType Junction -Value ${HOME}\.config\helix
+    }
+    $neovideConfDir = (Join-Path $env:APPDATA "neovide")
+    if (-not (Test-Path $neovideConfDir))
+    {
+        New-Item -Path $neovideConfDir -ItemType Junction -Value ${HOME}\.config\neovide
     }
 }
 
-if (-not ($isWindows)) {
+if (-not ($isWindows))
+{
     Write-Host "👍"
 }
