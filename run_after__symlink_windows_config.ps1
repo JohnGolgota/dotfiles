3002cea5f8ba55eb45e25e3a3eaa21c4c701e77a
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-01-08 10:35:20 -0500

    ya hice comedia, ya hice drama

--- run_after__symlink_windows_config.ps1
+++ run_after__symlink_windows_config.ps1
@@ -5,21 +5,32 @@ if ($IsWindows)
     {
         New-Item -Path $nvimConfDir -ItemType Junction -Value ${HOME}\.config\nvim
     }
+
     $rioConfDir = (Join-Path $env:LOCALAPPDATA "rio")
     if (-not (Test-Path $rioConfDir))
     {
         New-Item -Path $rioConfDir -ItemType Junction -Value ${HOME}\.config\rio
     }
+
     $helixConfDir = (Join-Path $env:APPDATA "helix")
     if (-not (Test-Path $helixConfDir))
     {
         New-Item -Path $helixConfDir -ItemType Junction -Value ${HOME}\.config\helix
     }
+
     $neovideConfDir = (Join-Path $env:APPDATA "neovide")
     if (-not (Test-Path $neovideConfDir))
     {
         New-Item -Path $neovideConfDir -ItemType Junction -Value ${HOME}\.config\neovide
     }
+
+
+    $alacrittyConfDir = (Join-Path $env:APPDATA "alacritty")
+    if (-not (Test-Path $alacrittyConfDir))
+    {
+        New-Item -Path $alacrittyConfDir -ItemType Junction -Value ${HOME}\.config\alacritty
+    }
+
 }
 
 if (-not ($isWindows))
