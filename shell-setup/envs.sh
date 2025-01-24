a41dcaeae98447de5a27848008f17473f4a1c513
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-01-24 09:56:24 -0500

    me esta dejando triste

--- shell-setup/envs.sh
+++ shell-setup/envs.sh
@@ -12,7 +12,7 @@ export GPG_TTY=$(tty)
 export DOTNET_ROOT="$HOME/.dotnet"
 
 # path
-export PATH="$PATH:$DOTNET_ROOT:/Applications/Docker.app/Contents/Resources/bin:$HOME/JS/bin:$HOME/asd/bin:$HOME/.dotnet/tools:$HOME/.local/share/bob/nvim-bin"
+# export PATH="$PATH:$DOTNET_ROOT:/Applications/Docker.app/Contents/Resources/bin:$HOME/JS/bin:$HOME/asd/bin:$HOME/.dotnet/tools:$HOME/.local/share/bob/nvim-bin"
 
 
 # darwin
