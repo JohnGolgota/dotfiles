f707210f9ab9d79d52682a7dfc04bdc402609e08
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-02-06 09:46:43 -0500

    no se porque sigo

--- dot_config/powershell/custom_moduls/Custom_PSReadLineKeyHandler.ps1
+++ dot_config/powershell/custom_moduls/Custom_PSReadLineKeyHandler.ps1
@@ -4,16 +4,21 @@ Set-PSReadLineKeyHandler -Chord Ctrl+e -ScriptBlock {
 }
 
 Set-PSReadLineKeyHandler -Chord Ctrl+q -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('exit')
-}
-
-Set-PSReadLineKeyHandler -Chord Ctrl+d -ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert('yazi')
     [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
+Set-PSReadLineKeyHandler -Chord Ctrl+d -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('exit')
+}
+
 Set-PSReadLineKeyHandler -Chord Ctrl+g -ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert('gitui')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
+Set-PSReadLineKeyHandler -Chord Ctrl+f -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('zi')
     [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
