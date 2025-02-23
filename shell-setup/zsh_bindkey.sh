8c5b1952734d7ffb0d1d4fc897f62f57c67549a5
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-02-22 19:05:09 -0500

    cambios
    borre una cosas que no uso
    add keybinds para zsh

---
+++ shell-setup/zsh_bindkey.sh
@@ -0,0 +1,27 @@
+open_editor() {
+    if [[ -n $EDITOR ]]; then
+        $EDITOR "$@"
+    else
+        echo "No editor set"
+    fi
+}
+
+zle -N open_editor
+bindkey '^E' open_editor
+
+open_yazi() {
+    yazi
+}
+
+zle -N open_yazi
+bindkey '^Q' open_yazi
+
+open_gitui() {
+    gitui
+}
+
+zle -N open_gitui
+bindkey '^G' open_gitui
+
+zle -N zi
+bindkey '^F' zi
