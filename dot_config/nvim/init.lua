c2e5e918708ada36fe8d7705e60a4f08157e7afc
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-10-02 09:25:08 -0500

    feat: lazy...
    como funcionaba antes... no se.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,4 @@
+require("config.lazy")
 require("kickstart")
 require("config")
 
---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,17 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if nor(vim.uv or vim.loop).fs_stat(lazypath) then
+   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+   if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+         { "Fail to clone lazy.nvim", "ErrorMsg" },
+         { out, "WarnignMsg" },
+         { "\nPress any key to exit" },
+      }, true, {})
+      vim.fn.getchar()
+      os.exit(1)
+   end
+end
+
+vim.opt.rtp:prepend(lazypath)
